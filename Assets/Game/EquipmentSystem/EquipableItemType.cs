869de07bf71305df014bebf83c0c4cd854fb76a8
Léo (FLC) <lcgonsalves@wpi.edu>
2023-11-06 00:11:32 +0100

    todos

--- Assets/Game/EquipmentSystem/EquipableItemType.cs
+++ Assets/Game/EquipmentSystem/EquipableItemType.cs
@@ -2,11 +2,16 @@ using UnityEngine;
 
 namespace Game.EquipmentSystem
 {
+    // TODO: Make this abstract
     [CreateAssetMenu(fileName = "Equipable Item", menuName = "GameItem/New EquipableItemType", order = 0)]
     public class EquipableItemType : UsableItemType
     {
         public override void Use(GameItemInventory user)
         {
+            // TODO: Make extension for projectile consumable item
+            // Using this item while equipped will fire a projectile
+            // make projectile do damage to dummy
+            // copy paste code from progress bar to make a health bar
             Debug.Log("Equipable items cannot be used yet.");
         }
     }
--- Assets/Game/EquipmentSystem/UsableItemType.cs
+++ Assets/Game/EquipmentSystem/UsableItemType.cs
@@ -3,7 +3,8 @@ using UnityEngine;
 
 namespace Game.EquipmentSystem
 {
-    
+    // TODO: Inherit this to make a heal potion
+    // TODO: implement health component (copy-paste)
     public abstract class UsableItemType : GameItemType
     {
         [InfoBox("The message to display when the item is used. Use $itemName to display the item name.")]
